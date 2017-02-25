12e897658eedbdb2cb05fb037516c2f47aef5001
Aaron Teague <aaron.teague@outlook.com>
2017-02-24 21:48:20 -0600

    Finally finished up that homework.

--- 03-angular/assignment-2/src/app/hero-detail.component.ts
+++ 03-angular/assignment-2/src/app/hero-detail.component.ts
@@ -1,29 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
-import {ActivatedRoute, Params } from '@angular/router';
-import {Location } from '@angular/common';
-
 import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
-import {HeroService } from './hero.service';
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
 
-import { Hero } from './hero';
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
-  ){}
-  @Input() hero: Hero;
+  ) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
   }
 
   goBack(): void {
